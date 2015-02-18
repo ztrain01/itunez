d2ef8871a3614b247b744db90d53a2d5b3a83d3e
Zander Allen <ztrain01@gmail.com>
2015-02-17 20:18:07 -0700

    working on service and controller

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -30,6 +30,11 @@ app.controller('mainCtrl', function($scope, itunesService){
   //Also note that that method should be retuning a promise, so you could use .then in this function.
     
     //Code here
+//  $scope.getSongData = function() {
+//    itunesService.getSong($scope.artist).then(function(songs){
+//      $scope.songData
+//    })
+//  }
 
 
   //Check that the above method is working by entering a name into the input field on your web app, and then console.log the result
